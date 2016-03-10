634fa881b2732af6dc2b9becc774104fde7a40b2
bettaieb med ali <medali.bettaieb@esprit.tn>
2016-03-10 11:27:40 +0100

    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

---
+++ mini-lms/src/main/java/entities/User.java
@@ -0,0 +1,40 @@
+package entities;
+
+import java.io.Serializable;
+import java.lang.Long;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@Entity
+
+public class User implements Serializable {
+
+	   
+	@Id
+	private Long id;
+	private String name;
+	private static final long serialVersionUID = 1L;
+
+	public User() {
+		super();
+	}   
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}   
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+   
+}
